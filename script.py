19042511f0c625fc7ebffc1d7034a1ffd79370fb
Enadaren <ghostoflaboratory@gmail.com>
2024-02-01 00:02:18 +0300

    New first value

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 import os
 a=int(input("Input under-root value: "))
 n=int(input("root-value: "))
-x0=a/2+1
-x1=a/2
+x0=a/n +1
+x1=a/n
+i=0
 while abs(x0-x1)>=0.0001:
     x0=x1
     x1=((n-1)*x0+a/(x0**(n-1)))/n
-print("root", n, "of", a, ":", '%.4f' % x1)
+    i=i+1
+print("root", n, "of", a, ":", '%.4f' % x1,"\n",i,"steps")
 
 
